bc67a2206b983634ca357f540428e5ee4458def2
lodmev <lodmev@gmail.com>
2023-12-25 13:38:37 +0300

    feat: begin dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!(members instanceof Array)) {
+    return false;
+  }
+  let res = '';
+  /* 
+   * @param {String}
+   */ 
+  function parseName(name) {
+    if (!(name typeof 'string')) {
+      return '';
+    }
+  }
 }
 
 module.exports = {
